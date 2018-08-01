3e63baf1457b06710db7b566a91ac4e47c03de2c
Robert Hailey <git@osndok.com>
2018-08-01 18:26:05 -0500

    f: not_today: add another button for "next week"

--- GTG/plugins/not_today/not_today.py
+++ GTG/plugins/not_today/not_today.py
@@ -24,13 +24,13 @@ class notToday:
     def __init__(self):
         self.plugin_api = None
         self.tb_button = None
+        self.tb_button2 = None
 
     def activate(self, plugin_api):
         self.plugin_api = plugin_api
         self.req = self.plugin_api.get_requester()
         self._init_gtk()
-        self.plugin_api.set_active_selection_changed_callback(
-            self.selection_changed)
+        self.plugin_api.set_active_selection_changed_callback(self.selection_changed)
 
     def mark_not_today(self, button):
         start_date = Date.parse("tomorrow")
@@ -38,11 +38,29 @@ class notToday:
             task = self.req.get_task(tid)
             task.set_start_date(start_date)
 
+    def task_cb(self, widget, plugin_api, task):
+        start_date = Date.parse("tomorrow")
+        task.set_start_date(start_date)
+        plugin_api.get_view_manager().close_task(task.get_id())
+
+    def mark_next_week(self, button):
+        start_date = Date.parse("+7d")
+        for tid in self.plugin_api.get_selected():
+            task = self.req.get_task(tid)
+            task.set_start_date(start_date)
+
+    def task_cb2(self, widget, plugin_api, task):
+        start_date = Date.parse("+7d")
+        task.set_start_date(start_date)
+        plugin_api.get_view_manager().close_task(task.get_id())
+
     def selection_changed(self, selection):
         if selection.count_selected_rows() > 0:
             self.tb_button.set_sensitive(True)
+            self.tb_button2.set_sensitive(True)
         else:
             self.tb_button.set_sensitive(False)
+            self.tb_button2.set_sensitive(False)
 
 ## GTK FUNCTIONS ##############################################################
     def _init_gtk(self):
@@ -59,12 +77,23 @@ class notToday:
         self.tb_button.show()
         self.plugin_api.add_toolbar_item(self.tb_button)
 
+        self.tb_button2 = gtk.ToolButton()
+        self.tb_button2.set_sensitive(False)
+        self.tb_button2.set_icon_name("media-skip-forward")
+        self.tb_button2.set_is_important(True)
+        next_week=_("Defer Till Next Week");
+        self.tb_button2.set_label(next_week);
+        self.tb_button2.set_tooltip_text(do_it_tomorrow);
+        self.tb_button2.connect('clicked', self.mark_next_week)
+        self.tb_button2.show()
+        self.plugin_api.add_toolbar_item(self.tb_button2)
+
     def onTaskOpened(self, plugin_api):
         # get the opened task
         task = plugin_api.get_ui().get_task()
 
         if task.get_status() == Task.STA_ACTIVE:
-            # add button
+            # add buttons
             self.taskbutton = gtk.ToolButton()
             #self.decide_button_mode(self.taskbutton, task)
             self.taskbutton.connect('clicked', self.task_cb, plugin_api, task)
@@ -73,10 +102,13 @@ class notToday:
             self.taskbutton.show()
             plugin_api.add_toolbar_item(self.taskbutton)
 
-    def task_cb(self, widget, plugin_api, task):
-        start_date = Date.parse("tomorrow")
-        task.set_start_date(start_date)
-        plugin_api.get_view_manager().close_task(task.get_id())
+            self.taskbutton2 = gtk.ToolButton()
+            #self.decide_button_mode(self.taskbutton2, task)
+            self.taskbutton2.connect('clicked', self.task_cb2, plugin_api, task)
+            self.taskbutton2.set_tooltip_text(_("Defer Till Next Week"));
+            self.taskbutton2.set_icon_name("media-skip-forward")
+            self.taskbutton2.show()
+            plugin_api.add_toolbar_item(self.taskbutton2)
 
     def deactivate(self, plugin_api):
         """ Remove Toolbar Button """
@@ -84,7 +116,17 @@ class notToday:
             if self.tb_button:
                 self.plugin_api.remove_toolbar_item(self.tb_button)
                 self.tb_button = False
+            if self.tb_button2:
+                self.plugin_api.remove_toolbar_item(self.tb_button2)
+                self.tb_button2 = False
         else:
             if self.taskbutton:
                 self.plugin_api.remove_toolbar_item(self.taskbutton)
                 self.taskbutton = False
+            if self.taskbutton2:
+                self.plugin_api.remove_toolbar_item(self.taskbutton2)
+                self.taskbutton2 = False
+
+
+
+
